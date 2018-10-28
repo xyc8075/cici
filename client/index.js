1885b7880842423809cfc67bc8556abf1277885c
xyc8075 <460754184@qq.com>
2018-10-28 23:18:08 +0800

    add input

--- client/index.js
+++ client/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { DatePicker } from 'antd';
+import { DatePicker, Input } from 'antd'
 
-import 'antd/lib/date-picker/style/css';
+import 'antd/lib/date-picker/style/css'
+import 'antd/lib/input/style/css'
 import './common/styles/reset.scss'
 import './index.scss'
 
 ReactDOM.render(
   <div>
     <DatePicker />
+    <Input />
   </div>,
   document.getElementById('app')
 )
